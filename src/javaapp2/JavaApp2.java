8ec3bcac2bd696bcd13a43760ea5157338870b5a
latif, vedic <vedicsoftcts@gmail.com>
2019-08-28 11:26:42 +0530

    Hello Java

---
+++ src/javaapp2/JavaApp2.java
@@ -0,0 +1,13 @@
+package javaapp2;
+
+public class JavaApp2 {
+
+    public static void main(String[] args) {
+        System.out.println("this is Java App2");
+        for(int i = 0; i<=10; i++)
+        {
+            System.out.println(i);
+        }
+    }
+    
+}
